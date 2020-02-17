7941e208c8e7c55fca94ba5c0386d048c727775b
EdenTho <edentardis@gmail.com>
2020-02-17 10:39:55 +0200

    Update tmdb.js

--- routes/api/tmdb.js
+++ routes/api/tmdb.js
@@ -1,10 +1,12 @@
 const rp = require('request-promise');
+const APIKEY = process.env.APIKEY;
+
 async function getMovieListByActorName(name){
-    const actorRes = await rp(`https://api.themoviedb.org/3/search/person?api_key=5e91bf33609f91a05655f2c9aa0ff7ae&query=` + encodeURIComponent(name));
+    const actorRes = await rp(`https://api.themoviedb.org/3/search/person?api_key=${APIKEY}&query=` + encodeURIComponent(name));
     let actorJson = JSON.parse(actorRes);
     let actorId = actorJson.results[0].id;
 
-    const response = await rp(`https://api.themoviedb.org/3/person/${actorId}/movie_credits?api_key=5e91bf33609f91a05655f2c9aa0ff7ae`);
+    const response = await rp(`https://api.themoviedb.org/3/person/${actorId}/movie_credits?api_key=${APIKEY}`);
     let json = JSON.parse(response);
     let movieIds = [];
 
@@ -29,7 +31,7 @@ function addReleaseYear(jsonArray){
 async function getMovieInfo(movieIds){
 let movieDetailsArray = [];
 for(const movieId of movieIds){
-    let response = await rp(`https://api.themoviedb.org/3/movie/${movieId}?api_key=5e91bf33609f91a05655f2c9aa0ff7ae`);
+    let response = await rp(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${APIKEY}`);
     response = JSON.parse(response);
     movieDetailsArray.push(response);
 
@@ -45,7 +47,7 @@ return movieDetailsArray;
 async function getRandomMovie(){
     let currentYear = new Date().getFullYear();
     let randomYear = randomNumber(1980, currentYear);
-    let movieRes = await rp(`https://api.themoviedb.org/3/discover/movie?api_key=5e91bf33609f91a05655f2c9aa0ff7ae&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=${randomYear}`);
+    let movieRes = await rp(`https://api.themoviedb.org/3/discover/movie?api_key=${APIKEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=${randomYear}`);
     let movieJson = JSON.parse(movieRes);
     let resArrayLength = movieJson.results.length;
     let randomIndex = randomNumber(1, resArrayLength+1) - 1;
